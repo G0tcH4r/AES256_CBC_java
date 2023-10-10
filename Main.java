a3cb7267b722d0c792d29fbf256c2a1a2b3f4026
G0ctH4r <linjiajia0228@126.com>
2023-10-10 13:46:06 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,81 @@
+import java.security.*;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // 原文:
+        String message = "Hello, world!";
+        System.out.println("Message: " + message);
+
+        // 256位密钥 = 32 bytes Key:
+        byte[] key = "1234567890abcdef1234567890abcdef".getBytes("UTF-8");
+
+        // 加密:
+        byte[] data = message.getBytes("UTF-8");
+        byte[] encrypted = encrypt(key, data);
+        System.out.println("Encrypted: " + Base64.getEncoder().encodeToString(encrypted));
+
+        // 解密:
+        byte[] decrypted = decrypt(key, encrypted);
+        System.out.println("Decrypted: " + new String(decrypted, "UTF-8"));
+    }
+
+    // 加密:
+    public static byte[] encrypt(byte[] key, byte[] input) throws GeneralSecurityException {
+        // 设置算法/工作模式CBC/填充
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        // 恢复秘钥对象
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+
+        // CBC模式需要生成一个16 bytes的initialization vector:
+        SecureRandom sr = SecureRandom.getInstanceStrong();
+        byte[] iv = sr.generateSeed(16);
+        System.out.println(Arrays.toString(iv));
+        IvParameterSpec ivps = new IvParameterSpec(iv);
+
+        // 初始化秘钥：操作模式、秘钥、IV参数
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivps);
+
+        // 加密
+        byte[] data = cipher.doFinal(input);
+
+        // IV不需要保密，把IV和密文一起返回:
+        return join(iv, data);
+    }
+
+    // 解密:
+    public static byte[] decrypt(byte[] key, byte[] input) throws GeneralSecurityException {
+        // 把input分割成IV和密文:
+        byte[] iv = new byte[16];
+        byte[] data = new byte[input.length - 16];
+
+        System.arraycopy(input, 0, iv, 0, 16);//IV
+        System.arraycopy(input, 16, data, 0, data.length);// 秘钥
+        System.out.println(Arrays.toString(iv));
+
+        // 解密:
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");// 秘钥对象
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");// 恢复秘钥
+        IvParameterSpec ivps = new IvParameterSpec(iv); // 恢复IV
+
+        // 初始化秘钥:操作模式、秘钥、IV参数
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivps);
+
+        // 解密参数
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] join(byte[] bs1, byte[] bs2) {
+
+        byte[] r = new byte[bs1.length + bs2.length];
+        System.arraycopy(bs1, 0, r, 0, bs1.length);
+        System.arraycopy(bs2, 0, r, bs1.length, bs2.length);
+        return r;
+    }
+}
